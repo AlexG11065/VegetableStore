19a9fa55a41de5ed7ef8553011ce5bb23c88c254
Александр <berdawkevi4@gmail.com>
2024-04-26 12:13:05 +0300

    сайт устойчив

--- home/urls.py
+++ home/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-
-
 from .views import IndexShopView
 
 
+app_name = "home"
+
 urlpatterns = [
-  path('', IndexShopView.as_view()),# Пустая строка означает, что название пути будет соединяться с корневым путем загружаемым из корневого маршрута.
+  path('', IndexShopView.as_view(), name="index"),# Пустая строка означает, что название пути будет соединяться с корневым путем загружаемым из корневого маршрута.
 
 
 ]
